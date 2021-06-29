1269f77d2e780ea583160b7a2f7a7c4fdf9cee65
jngminj <exorbitance@naver.com>
2021-06-30 00:57:13 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-// Search
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '')
-});
 
 // Swiper
 new Swiper('.promotion .swiper-container', {
@@ -45,9 +31,6 @@ spyEls.forEach(function(spyEl){
   .addTo(new ScrollMagic.Controller());
 });
 
-// thisYear
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
 
 // to-top 버튼
 const toTopEl = document.querySelector('#to-top')
